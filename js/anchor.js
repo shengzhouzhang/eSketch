53571198174aa64b65c114b23fb96d4553fa2e0d
szha246 <szhang@ShengzhouZhangs-MacBook-Pro.local>
2013-08-21 13:38:08 +1000

    anchor

--- js/anchor.js
+++ js/anchor.js
@@ -1,7 +1,10 @@
 var Anchor = function(canves) {
   
   var activitedPoints = [];
-  var MagnetRadius = 20;
+  var MagnetRadius = 30;
+  var MagentField = [];
+  var MagentFieldStatus = "undraw";
+  this.MagnetStatus = "notready";
   
   this.light = function(point) {
     
@@ -35,7 +38,7 @@ var Anchor = function(canves) {
     anchor.light(point);
     
     // hover event
-    point.hover(function(){
+    point.hover(function() {
       
       if (point.status !== "activited") {
         
@@ -53,31 +56,45 @@ var Anchor = function(canves) {
       
       activitedPoints.push(this);
       
-      if(activitedPoints.length > 2) {
+      if(activitedPoints.length === 2 && activitedPoints[0].set === this.set) {
+          
+        activitedPoints.splice(1, 1);  
         
-        anchor.light(activitedPoints[0]);
+      } else if (activitedPoints.length > 2 && activitedPoints[1].set === this.set) {
         
-        activitedPoints.splice(0, 1);
+        activitedPoints.splice(2, 1);
+      } else {
+            
+        console.log(2);
+        
+        if (activitedPoints.length > 2) {
+          
+          anchor.light(activitedPoints[0]);
+          activitedPoints.splice(0, 1);
+        }
+        
+        if (point.status !== "activited") {
+        
+          anchor.activite(point);
+        
+        } else {
+        
+          point.status = "hover";
+        }
+        
+        anchor.MagnetReset();
       }
       
-      if (point.status !== "activited") {
-        
-        anchor.activite(point);
-        
-      } else {
-        
-        point.status = "hover";
-      }
     });
     
     point.positionX = function() {
    
-      return point.attrs.cx + point.set[0].ft.attrs.translate.x;
+      return point.matrix.x(point.attrs.cx, point.attrs.cy);
     }
       
     point.positionY = function() {
    
-      point.attrs.cy + point.set[0].ft.attrs.translate.y;
+      return point.matrix.y(point.attrs.cx, point.attrs.cy);
     }
     
     return point;
@@ -85,14 +102,127 @@ var Anchor = function(canves) {
   
   this.Magnet = function() {
     
-    if (activitedPoints.length === 2) {
+    if (activitedPoints.length === 2 && (activitedPoints[0].set[0].ft.status === "activited" || activitedPoints[1].set[0].ft.status === "activited")) {
       
-      //console.log(activitedPoints[0]);
-      
-      if (Math.abs(activitedPoints[0].positionX() - activitedPoints[1].positionX()) < MagnetRadius || Math.abs(activitedPoints[0].positionY() - activitedPoints[1].positionY()) < MagnetRadius) {
+      if (Math.abs(activitedPoints[0].positionX() - activitedPoints[1].positionX()) < MagnetRadius && Math.abs(activitedPoints[0].positionY() - activitedPoints[1].positionY()) < MagnetRadius) {
         
-        console.log("haha");
+        if (this.MagnetStatus !== "ready")
+          this.MagnetStatus = "ready";
+       
+      } else {
+        
+        if (this.MagnetStatus !== "notready")
+          this.MagnetStatus = "notready";
       }
+      
+      this.drawMagnetField();
     }
   };
+  
+  this.MagnetDistance = function() {
+    
+    var movePoint;
+    var waitPoint;
+    
+    activitedPoints.forEach(function(point) {
+    
+      if (point.set[0].ft.status === "activited") {
+        
+        movePoint = point;
+      } else {
+        
+        waitPoint = point;
+      }
+    });
+    
+    var distance = {};
+    
+    distance.x = waitPoint.positionX() - movePoint.positionX();
+    distance.y = waitPoint.positionY() - movePoint.positionY();
+
+    return distance;
+  };
+  
+  this.MagnetReset = function() {
+   
+    this.MagnetStatus = "notready";
+    this.deleteMagentField();
+  };
+  
+  this.drawMagnetField = function() {
+    
+    if (MagentFieldStatus === "undraw") {
+      
+      activitedPoints.forEach(function(point) {
+        
+        if (point.set[0].ft.status !== "activited") {
+          
+          MagentField.push(canves.circle(point.positionX(), point.positionY(), MagnetRadius).attr({"stroke-dasharray": "--"}));
+          
+          MagentFieldStatus = "draw";
+          
+          return;
+            }
+      });
+    }
+  };
+  
+  this.deleteMagentField = function() {
+    
+    if (MagentFieldStatus === "draw") {
+      
+      MagentField.forEach(function(field){
+      
+        field.remove();
+      });
+      
+      MagentFieldStatus = "undraw";
+    }
+  };
+    
+    
+  this.join = function() {
+    
+    if (activitedPoints.length !== 2)
+      return;
+    
+    if (activitedPoints[0].set.superset === undefined || activitedPoints[1].set.superset === undefined) {
+      
+      var superset = canves.set();
+      
+      superset.push(activitedPoints[0].set);
+      superset.push(activitedPoints[1].set);
+      
+      activitedPoints[0].set[0].ft.hideHandles({undrag: true});
+      activitedPoints[1].set[0].ft.hideHandles({undrag: true});
+      
+      var ftOptions = {draw: ["bbox"], scale: ["bboxCorners", "bboxSides"], rotate: ["axisX"], drag: ["center", "self"], keepRatio: ["bboxCorners"]};
+      
+      superset.ft = canves.freeTransform(superset, ftOptions, function(ft, events) {
+        
+      });
+      
+      activitedPoints[0].set.superset = superset;
+      activitedPoints[1].set.superset = superset;
+      
+    } else if (activitedPoints[0].set.superset !== undefined) {
+      
+      activitedPoints[0].set.superset.push(activitedPoints[1]);
+      
+    } else if (activitedPoints[1].set.superset !== undefined) {
+      
+      activitedPoints[1].set.superset.push(activitedPoints[0]);
+      
+    } else if (activitedPoints[0].set.superset !== activitedPoints[1].set.superset) {
+      
+      activitedPoints[0].set.superset.forEach(function(item) {
+        
+        activitedPoints[1].set.superset.push(item);
+      });
+      
+      activitedPoints[0].set.superset.clear();
+    }
+    
+  }
+    
 }
--- js/image.js
+++ js/image.js
@@ -11,6 +11,7 @@ var Image = function(canves) {
     
     var set = canves.set();
     set.push(img);
+    img.set = set;
     
     // define action points
     points.forEach(function(point) {
@@ -30,7 +31,8 @@ var Image = function(canves) {
       item.find("span.scaleX").html(img.ft.attrs.scale.x.toFixed(1));
       item.find("span.scaleY").html(img.ft.attrs.scale.y.toFixed(1));
       
-      anchorFactory.Magnet();
+      if (events[0] === "drag start" || events[0] === "drag")
+        anchorFactory.Magnet();
     });
     
     img.ft.hideHandles({undrag: false});
@@ -45,30 +47,55 @@ var Image = function(canves) {
     
     img.mousedown(function(){
     
-      img.ft.status = "activited";
+      if (img.set.superset === undefined) {
+        
+        console.log(img.set);
+        img.ft.status = "activited";
+        
+      }
     });
     
     // define the click function
     img.mouseup(function(event) {
       
-      img.ft.showHandles();
-      
-      
-      // close all images' handles
-      images.forEach(function(set) {
+      if (img.set.superset === undefined) {
         
-        if(event.target !== set[0][0])
-          set[0].ft.hideHandles({undrag: false});
-      });
-      
-      $("ul.attributes > li").removeClass("activited");
-      $("ul.attributes > li:eq(" + img.ft.id + ")").addClass("activited");
+        img.ft.showHandles();
+        
+        // close all images' handles
+        images.forEach(function(set) {
+          
+          if(event.target !== set[0][0])
+            set[0].ft.hideHandles({undrag: false});
+        });
+        
+        $("ul.attributes > li").removeClass("activited");
+        $("ul.attributes > li:eq(" + img.ft.id + ")").addClass("activited");
+        
+        if (anchorFactory.MagnetStatus === "ready") {
+          
+          var distance = anchorFactory.MagnetDistance();
+          
+          img.ft.attrs.translate.x += distance.x;
+          img.ft.attrs.translate.y += distance.y;
+          
+          img.ft.apply();
+          
+          anchorFactory.join();
+          
+        }
+        
+        img.ft.status = "normal";
+        anchorFactory.MagnetReset();
+        
+      }
       
     });
     
-    
     return img;
   };
+  
+  
 
 };
 
