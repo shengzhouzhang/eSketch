2cf6aaf2e95b3bd3fb8c8862e9f622f128637e99
szha246 <szhang@ShengzhouZhangs-MacBook-Pro.local>
2013-10-25 13:55:07 +1100

    scale

--- js/data.js
+++ js/data.js
@@ -1,19 +1,19 @@
 var equipmentsData = [
-  {type: "equipment", name: "Boom", layer: 1, url: "images/sample_1.png", x: 10, y: 280, width: 300, length: 70, anchors: [
-    {layer: 1, x: 8, y: 64, radius: 4},
-    {layer: 1, x: 155, y: 11.5, radius: 4},
-    {layer: 1, x: 115.5, y: 6, radius: 4},
-    {layer: 1, x: 294, y: 64, radius: 4}
+  {type: "equipment", name: "Boom", layer: 1, url: "images/sample_1.png", x: 10, y: 280, width: 360, length: 100, anchors: [
+    {layer: 1, x: 8, y: 92, radius: 4},
+    {layer: 1, x: 185, y: 16, radius: 4},
+    {layer: 1, x: 138, y: 8, radius: 4},
+    {layer: 1, x: 353, y: 92, radius: 4}
   ], grade: 2,
    youtube: "http://www.youtube.com/watch?v=u6QIpXca9D4",
    wikipedia: "http://en.wikipedia.org/wiki/Backhoe"
   },
-  {type: "equipment", name: "Stick", layer: 1, url: "images/sample_2.png", x: 180, y: 430, width: 175, length: 50, anchors: [
-    {layer: 1, x: 5, y: 45, radius: 3},
-    {layer: 1, x: 26, y: 43, radius: 3},
-    {layer: 1, x: 130, y: 41, radius: 3},
-    {layer: 1, x: 170, y: 25, radius: 3},
-    {layer: 1, x: 125, y: 7, radius: 3}
+  {type: "equipment", name: "Stick", layer: 1, url: "images/sample_2.png", x: 180, y: 430, width: 217, length: 65, anchors: [
+    {layer: 1, x: 7, y: 57, radius: 4},
+    {layer: 1, x: 31, y: 54, radius: 4},
+    {layer: 1, x: 161, y: 54, radius: 4},
+    {layer: 1, x: 210, y: 33, radius: 4},
+    {layer: 1, x: 155, y: 8, radius: 4}
   ], grade: 3,
    youtube: "http://www.youtube.com/watch?v=u6QIpXca9D4",
    wikipedia: "http://en.wikipedia.org/wiki/Backhoe"
@@ -34,18 +34,18 @@ var equipmentsData = [
   {type: "equipment", name: "Link", layer: 1, url: "images/sample_4.png", x: 370, y: 300, width: 60, length: 50, anchors: [
     {layer: 1, x: 10, y: 41, radius: 4}
   ], grade: 6},
-  {type: "equipment", name: "Bucket", layer: 1, url: "images/sample_5.png", x: 10, y: 420, width: 60, length: 80, anchors: [
-    {layer: 1, x: 20, y: 74, radius: 3},
-    {layer: 1, x: 39, y: 69, radius: 3},
-    {layer: 1, x: 60, y: 40, radius: 3}
+  {type: "equipment", name: "Bucket", layer: 1, url: "images/sample_5.png", x: 10, y: 420, width: 80, length:90, anchors: [
+    {layer: 1, x: 28, y: 82, radius: 4},
+    {layer: 1, x: 50, y: 78, radius: 4},
+    {layer: 1, x: 80, y: 40, radius: 4}
   ], grade: 4,
    youtube: "http://youtu.be/MCc6eK56afw",
    wikipedia: "http://en.wikipedia.org/wiki/Bucket_%28machine_component%29"
   },
-  {type: "equipment", name: "Upper-Carriage", layer: 1, url: "images/sample_6.png", x: 240, y: 520, width: 250, length: 125, anchors: [
-    {layer: 1, x: 65, y: 105, radius: 5, opacity: 0.5},
-    {layer: 1, x: 100, y: 95, radius: 5, opacity: 0.5},
-    {layer: 1, x: 90, y: 125, radius: 5, opacity: 0.5}
+  {type: "equipment", name: "Upper-Carriage", layer: 1, url: "images/sample_6.png", x: 240, y: 520, width: 250, length: 115, anchors: [
+    {layer: 1, x: 65, y: 95, radius: 5, opacity: 0.5},
+    {layer: 1, x: 100, y: 85, radius: 5, opacity: 0.5},
+    {layer: 1, x: 90, y: 115, radius: 5, opacity: 0.5}
   ], slots: [
     {layer: 2, x: 150, y: 52, width: 120, height: 70, opacity: 0.5, text: "Click Me"}
   ], grade: 0,
--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
       otherLayer.push(item);
   });
   
-  //var background = new Background(canvas, {url: "images/background.png", x: 0, y: 370, width: "110%", height: 310, components: [{layer: 1, url: "images/truck.png", x:150, y: 150, width: 200, length: 100}]});
+  var background = new Background(canvas, {url: "images/background.png", x: 0, y: 370, width: "110%", height: 310, components: [{layer: 1, url: "images/truck.png", x:150, y: 150, width: 200, length: 100}]});
   
-  var loader = new Loader(panel);
+  //var loader = new Loader(panel);
   
   //$("table.info").hide();
   $("table.info").draggable();
